9c6e409733045c209e2296aefe85ff34e9514621
omarabdelfattah <omar.mero200125@gmail.com>
2023-07-18 17:55:19 +0300

    add the basic routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// login 
+Route::post('/login', [AccountController::class, 'login'])->name('login');
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    // logout
+    Route::get('/logout', [AccountController::class, 'logout'])->name('logout');
+
+    // tasks routes group
+    Route::group(['prefix' => 'tasks'] ,function(){
+        Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+        Route::post('/', [TaskController::class, 'store'])->name('tasks.store');
+        Route::get('/{id}', [TaskController::class, 'show'])->name('tasks.show');
+        Route::put('/{id}', [TaskController::class, 'update'])->name('tasks.update');
+        Route::delete('/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+    });
+
 });
+
+ 
