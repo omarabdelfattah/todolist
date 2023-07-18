298489eb9b909114109d4555a87cab296a9caa22
omarabdelfattah <omar.mero200125@gmail.com>
2023-07-18 17:42:01 +0300

    make some migrations

--- database/migrations/2023_07_18_143348_create_tasks.php
+++ database/migrations/2023_07_18_143348_create_tasks.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\User;
 return new class extends Migration
 {
     /**
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->foreignIdFor(\App\Models\Role::class);
+            $table->text('description')->nullable(false);
+            $table->boolean('completed')->default(false);
+            $table->softDeletes();
+            $table->foreignIdFor(User::class);
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tasks');
     }
 };
--- database/migrations/2023_07_18_143725_create_permissions.php
+++ database/migrations/2023_07_18_143725_create_permissions.php
@@ -11,15 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('permissions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
-            $table->foreignIdFor(\App\Models\Role::class);
+            $table->text('name')->nullable(false);
+            $table->boolean('add')->default(false);
+            $table->boolean('edit')->default(false);
+            $table->boolean('delete')->default(false);
+            $table->boolean('view')->default(false);
         });
     }
 
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permissions');
     }
 };
